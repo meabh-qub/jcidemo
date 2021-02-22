81fcb6b5409a55d05f5e63947ec3a75ed5027bb2
Leona <leona@leonas-imac.home>
2021-02-22 11:19:32 +0000

    Adding print statements

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -2,10 +2,12 @@ package uk.ac.qub.eeecs.dave;
 
 public class Calculator {
 	public int add(int a, int b) {
+		System.out.println("Add finished");
 		return a+b;
 	}
 	
 	public int sub(int a, int b) {
+		System.out.println("Subtract finished");
 		return a-b;
 	}
 	
